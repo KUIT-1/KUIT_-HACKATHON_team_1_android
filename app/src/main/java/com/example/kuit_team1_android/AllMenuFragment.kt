6f0d369aa7e6cf494bf156f3a7de5a06a9a8ed00
jiw0n <>
2023-06-23 17:16:19 +0900

    menu 구분

--- app/src/main/java/com/example/kuit_team1_android/AllMenuFragment.kt
+++ app/src/main/java/com/example/kuit_team1_android/AllMenuFragment.kt
@@ -1,6 +1,8 @@
 package com.example.kuit_team1_android
 
+import android.icu.lang.UCharacter.GraphemeClusterBreak.L
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,6 +19,8 @@ class AllMenuFragment : Fragment() {
     var allList : ArrayList<HomeItem> = arrayListOf()
     var foodList : ArrayList<HomeItem> = arrayListOf()
     var productList : ArrayList<HomeItem> = arrayListOf()
+    var homeItem = ArrayList<HomeItem>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,45 +32,66 @@ class AllMenuFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initDummyData()
+
         starbucksMenuAdapter = starbucksMenuAdapter(drinkList)
+
         binding.categoryDrink.setOnClickListener {
+            Log.d("ClickEvent", "drink")
+            Log.d("ListSize", drinkList.size.toString())
+
             starbucksMenuAdapter = starbucksMenuAdapter((drinkList))
+            binding.starbucksMenuListRv.adapter = starbucksMenuAdapter
+
             starbucksMenuAdapter((drinkList)).notifyDataSetChanged()
         }
         binding.categoryFood.setOnClickListener {
+            Log.d("ClickEvent", "food")
+            Log.d("FoodListSize", foodList.size.toString())
+
+
             starbucksMenuAdapter = starbucksMenuAdapter((foodList))
+            binding.starbucksMenuListRv.adapter = starbucksMenuAdapter
+
             starbucksMenuAdapter((foodList)).notifyDataSetChanged()
         }
         binding.categoryProduct.setOnClickListener {
+            Log.d("ClickEvent", "product")
+
             starbucksMenuAdapter = starbucksMenuAdapter((productList))
+            binding.starbucksMenuListRv.adapter = starbucksMenuAdapter
+
             starbucksMenuAdapter((productList)).notifyDataSetChanged()
         }
+
         binding.starbucksMenuListRv.adapter = starbucksMenuAdapter
         binding.starbucksMenuListRv.layoutManager = GridLayoutManager(context, 1)
-//        starbucksMenuListRv!!.setOnItemClickListener(object : WebtoonAdapter.OnItemClickListener{
-//            override fun onItemClick(webtoonInfo: WebtoonCover) {
-//                val webtoonbundle = Bundle()
-//                val webToonDeatilFragment = WebtoonDetailFragment()
-//                val dataJson = Gson().toJson(webtoonInfo)
-//                webtoonbundle.putString("webtoonInfo", dataJson)
-//                webToonDeatilFragment.arguments = webtoonbundle
-//                parentFragment!!.parentFragmentManager.beginTransaction()
-//                    .replace(R.id.main_frm, webToonDeatilFragment).commit()
-//
-//            }
-//
-//        })
     }
     private fun initDummyData() {
         val dummydata = HomeItem(R.drawable.img,"아메리카노","달달",1)
-        drinkList.add(dummydata)
+//        drinkList.add(dummydata)
+
         allList.add(dummydata)
         val dummydata2 = HomeItem(R.drawable.img,"아메리카노2","달달",2)
-        foodList.add(dummydata2)
+//        foodList.add(dummydata2)
+
         allList.add(dummydata2)
         val dummydata3 = HomeItem(R.drawable.img,"아메리카노3","달달",3)
-        productList.add(dummydata3)
+//        productList.add(dummydata3)
+
         allList.add(dummydata3)
+
+        for (i in 0..allList.size-1){
+            if(allList[i].menuType==1){
+                drinkList.add(allList[i])
+            }else if (allList[i].menuType==2){
+                foodList.add(allList[i])
+            } else{
+                productList.add(allList[i])
+            }
+        }
+
+
         starbucksMenuAdapter?.notifyDataSetChanged()
     }
+
 }
--- app/src/main/java/com/example/kuit_team1_android/RetrofitInterface.kt
+++ app/src/main/java/com/example/kuit_team1_android/RetrofitInterface.kt
@@ -1,21 +1,20 @@
 package com.example.kuit_team1_android
 
-import com.example.naverwebtoon.data.Member
-import com.example.naverwebtoon.data.MemberResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import java.lang.reflect.Member
 
 interface RetrofitInterface {
-    @POST("/members")
+    @POST("/members") //변경필요
     fun signUp(
-        @Body member: Member
-    ): Call<MemberResponse>
+        @Body homeItem: HomeItem
+    ): Call<HomeItem>
 
-    @GET("/members/{id}")
+    @GET("/members/{id}") //변경필요
     fun getMember(
         @Path("id") id : Int
-    ): Call<MemberResponse>
+    ): Call<HomeItem>
 }
